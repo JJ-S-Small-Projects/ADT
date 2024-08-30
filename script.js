837b6c6f4f6ada02c20d604d2f320140a31eda3c
Jonathan Juining <Jonathan.Juining@AirdroiTech.com>
2024-08-30 10:08:37 +0800

    tes

--- script.js
+++ script.js
@@ -133,8 +133,8 @@ function displayFinalSurprise() {
     <div>
       <h2>Airdroitech Presents...</h2>
       <div class="video-container">
-  <iframe id="surprise-video" src="https://www.youtube.com/embed/EuciKoS6Hms?autoplay=1&fs=1&enablejsapi=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-</div>
+       <iframe id="surprise-video" src="https://www.youtube.com/embed/EuciKoS6Hms?autoplay=1&fs=1&enablejsapi=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
 
     </div>
   `;
